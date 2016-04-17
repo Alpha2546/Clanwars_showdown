f85e0709e8008298e1f2aad539ebd73b4485f051
Alpha2546 <adriaanvanloon@hotmail.com>
2016-04-17 23:50:40 +0200

    Filling up the map pack

---
+++ ui/mods/clanwars_showdown/maplist.js
@@ -0,0 +1,8 @@
+﻿cShareSystems.load_pas("Clanwars", [
+	"coui://ui/mods/clanwars_showdown/systems/bang_battle.pas",	
+	"coui://ui/mods/clanwars_showdown/systems/burnt_playground.pas",	
+	"coui://ui/mods/clanwars_showdown/systems/marshalls_lament_titans.pas",	
+	"coui://ui/mods/clanwars_showdown/systems/medea.pas",	
+	"coui://ui/mods/clanwars_showdown/systems/mots_recipe_for_disaster.pas",		
+	"coui://ui/mods/clanwars_showdown/systems/twbaby.pas"	
+]);
